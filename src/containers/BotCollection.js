0f2ad4408713581cca2695b2d8cc6af542a496a5
Roysan Easo <easo.roysan@gmail.com>
2019-07-24 10:03:39 -0500

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -8,7 +8,7 @@ class BotCollection extends React.Component {
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.props.bots.map( bot => <BotCard key={bot.id} bot={bot} handleEnlistment={this.props.handleEnlist} />)}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,14 +1,42 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state={
+    bots: [],
+    yourBots: []
+  }
+
   render() {
     return (
       <div>
+        <YourBotArmy yourBots={this.state.yourBots} handleRemove={this.handleRemove} />
+        <BotCollection bots={this.state.bots} handleEnlist={this.handleEnlist} />
         {/* put your components here */}
       </div>
     );
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(r => r.json())
+    .then(bots => this.setState({ bots }))
+  }
+
+  handleEnlist = (desiredBot) => {
+    let yourIDs = this.state.yourBots.map((bot)=> bot.id)
+    if(yourIDs.includes(desiredBot.id)){
+      alert("You already have that bot in your army.")
+    }else{
+      this.setState({ yourBots: [...this.state.yourBots, desiredBot]})
+    }
+  }
+
+  handleRemove = (desiredBot) => {
+    this.setState({ yourBots: this.state.yourBots.filter(bot => bot.id !== desiredBot.id)})
   }
 
 }
