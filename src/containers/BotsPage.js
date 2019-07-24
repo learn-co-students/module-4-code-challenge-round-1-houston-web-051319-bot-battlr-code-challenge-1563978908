2aeb179bc77c590611382e4ba3c2c2a3f7053b98
BobNarwhalKing <danceflurry4366@gmail.com>
2019-07-24 10:14:52 -0500

    Checkpoint!

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,44 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  // state={
+  //   userBots: []
+  // }
+  
+  state={
+    bots: []
+  }
 
+  addUserBots = (newBot)=>{
+    this.setState({
+      bots: this.state.bots.map(bot => {
+        if(bot.id === newBot.id){
+          return {...bot, userBot: !bot.userBot}
+        } else {
+          return bot
+        }
+      })
+    })
+  }
+
+  componentDidMount(){
+	  fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+		.then(res=> res.json())
+			.then(bots=>{
+				this.setState({
+					bots: bots
+				})
+			})
+	}
   render() {
     return (
       <div>
         {/* put your components here */}
+        <YourBotArmy bots={this.state.bots}/>
+        <BotCollection bots={this.state.bots} addUserBots={this.addUserBots}/>
       </div>
     );
   }
