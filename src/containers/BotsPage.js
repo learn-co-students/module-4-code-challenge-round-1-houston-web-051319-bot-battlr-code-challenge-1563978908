259c38cc9097d0e41f5719119d9e40e58c1130de
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-07-24 10:18:16 -0500

    Finished up to checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,42 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    allBots: [],
+    armyBots: []
+  }
+
+  componentDidMount = () => {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots').then( res => {
+      return res.json()
+    }).then( json => {
+      this.setState({allBots: json})
+    })
+  }
+
+  handleAddToArmy = (bot) => {
+    if (this.state.armyBots.filter(currBot => currBot.id === bot.id).length < 1) {
+      let updatedArmyBotsArr = this.state.armyBots.concat(bot)
+      this.setState({armyBots: updatedArmyBotsArr})
+    }
+  }
+
+  handleRemoveFromArmy = (bot) => {
+    let updatedArmyBotsArr = this.state.armyBots.filter(currBot => {
+      return currBot.id !== bot.id
+    })
+    this.setState({armyBots: updatedArmyBotsArr})
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        {<YourBotArmy armyBots={this.state.armyBots} handleClick={this.handleRemoveFromArmy}/>}
+        {<BotCollection allBots={this.state.allBots} handleClick={this.handleAddToArmy}/>}
       </div>
     );
   }
